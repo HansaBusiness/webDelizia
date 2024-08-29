108693e153f65fbe26b9170b7b05d60d125ef3dd
Luchex <lquisbert@hansa.com.bo>
2024-08-29 01:16:07 -0400

    cambios

--- proyDelizia/src/app/app.component.spec.ts
+++ proyDelizia/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'delizia' title`, () => {
+  it(`should have as title 'proyDelizia'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('delizia');
+    expect(app.title).toEqual('proyDelizia');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, delizia');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, proyDelizia');
   });
 });
